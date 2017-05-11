0bf0b878bdae243c98b45ba10943d87d73a27aaf
NguyenTuanSieu <dev.phpjs@gmail.com>
2017-05-12 05:15:54 +0700

    .

---
+++ common/helpers.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: devphpjs
+ * Date: 5/12/2017
+ * Time: 5:10 AM
+ */
+
+if (!function_exists('xxx')):
+    /**
+     * @return string
+     */
+    function xxx()
+    {
+        return 'abc';
+    }
+endif;
